dfba8b1bd6377e3a8589659ff3291591c46d8369
Christopher Siem <christophersiem@Christophers-MacBook-Pro.local>
2020-05-22 10:56:36 +0200

    Git init

---
+++ src/main/java/Product.java
@@ -0,0 +1,4 @@
+package de.neuefische.model.db.utilis;
+
+public class Product {
+}
---
+++ src/main/java/ProductDb.java
@@ -0,0 +1,6 @@
+import java.util.ArrayList;
+
+public class ProductDb {
+
+    private final ArrayList <Product> productArray = new ArrayList<Product>();
+}
